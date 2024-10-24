a4cca871b3936a2233916a1940f61c0624aee870
Chayana Sat <vl4d.ivanovivanov@yandex.ru>
2024-10-24 22:38:59 +0800

    Добавлен header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './styles/App.css';
+import logo from './logo.png';
+import CarouselComponent from './components/CarouselComponent';
+import { Helmet } from 'react-helmet';
+import { Link } from 'react-router-dom';
 
-function App() {
+function App({token, setToken}) {
+  const [username, setUsername] = useState('');
+  const handleLogout = () => {
+    setToken('');
+    localStorage.removeItem('token');
+    localStorage.removeItem('username');
+    setUsername('');
+  };
+  useEffect(() => {
+    if (token) {
+      localStorage.setItem('token', token);
+      const storedUsername = localStorage.getItem('username');
+      if (storedUsername) {
+        setUsername(storedUsername);
+      }
+    } else {
+      localStorage.removeItem('token');
+      localStorage.removeItem('username');
+      setUsername('');
+    }
+  }, [token]);
+
   return (
     <div className="App">
+      <Helmet>
+        <title>МастерЛаб - открывай науку шаг за шагом!</title>
+      </Helmet>
       <header className="App-header">
-        <h1>Welcome to React!</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container">
+          <div className="logo">
+            <a href="/">
+              <img src={logo} alt="Логотип" className="logo"></img>
+            </a>
+          </div>
+          <nav>
+            <ul class="nav-links">
+              <Link to="/" className="nav-link">ГЛАВНАЯ</Link>
+              <Link to="/news" className="nav-link">НОВОСТИ</Link>
+              <Link to="/about" className="nav-link">О НАС</Link>
+              <Link to="/experiments" className="nav-link">ЭКСПЕРИМЕНТЫ</Link>
+            </ul>
+          </nav>
+          <div class="login">
+            {token ? (
+              <><span className = "username">Привет, {username}!</span>
+              <button className='login-btn' onClick={handleLogout}><b>ВЫЙТИ</b></button></>
+            ) : (
+            <Link to="/login">
+                <button class="login-btn"><b>ВХОД / РЕГИСТРАЦИЯ</b></button>
+            </Link>
+            )}
+          </div>
+        </div>
       </header>
     </div>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,78 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Experiments from './Experiments';
+import News from './News';
+import NewsDetail from './NewsDetail';
+import Login from './Login';
+import Register from './Register';
+import About from './About';
+import PrivacyPolicy from './PrivacyPolicy';
+import ProtectedRoute from './ProtectedRoute';
+import AddExperiment from './AddExperiment';
+import ExperimentDetails from './ExperimentDetails';
+import NotFound from './NotFound';
+import AdminPanel from './AdminPanel';
+
+const Main = () => {
+
+  const [token, setToken] = useState(localStorage.getItem('token') || '');
+  const userId = localStorage.getItem('userId');
+  const [username, setUsername] = useState(localStorage.getItem('username') || '');
+
+  // Функция для выхода из системы
+  const handleLogout = () => {
+    setToken('');
+    setUsername('');
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('username');
+  };
+
+  // Сохраняем токен в локальное хранилище при его изменении
+  useEffect(() => {
+    if (token) {
+      localStorage.setItem('token', token);
+    } else {
+      localStorage.removeItem('token');
+    }
+  }, [token]);
+
+  useEffect(() => {
+    if (username) {
+      localStorage.setItem('username', username);
+    } else {
+      localStorage.removeItem('username');
+    }
+  }, [username]);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App token={token} Home username={username} setToken={setToken} />} />
+        <Route path="/news" element={<News token={token} setToken={setToken} username={username} />} />
+        <Route path="/experiments" element={<Experiments token={token} setToken={setToken} userId={userId} username={username} />} />
+        <Route path="/login" element={<Login setToken={setToken} />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/policy" element={<PrivacyPolicy token={token} username={username} setToken={setToken} />} />
+        <Route path="/about" element={<About token={token} username={username} setToken={setToken} />} />
+        <Route path="/new_experiment" element={<ProtectedRoute token={token}><AddExperiment token={token} setToken={setToken} /></ProtectedRoute>} />
+        <Route path="/experiments/:id" element={<ExperimentDetails token={token} setToken={setToken} username={username}/>} />
+        <Route path="*" element={<NotFound token={token} setToken={setToken} username={username}/>} />
+        <Route path="/admin" element={<AdminPanel token={token} setToken={setToken} username={username}/>} />
+        <Route path="/news/:id" element={<NewsDetail token = {token} setToken={setToken} username={username}/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>
 );
 
